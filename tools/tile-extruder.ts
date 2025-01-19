015e93aca2f75a9b8f3994d49d0af4142c476ea1
Robouste <robindenis88@gmail.com>
2025-01-19 13:24:45 +0100

    - extruded animations

--- tools/tile-extruder.ts
+++ tools/tile-extruder.ts
@@ -1,17 +1,19 @@
 import { readdirSync } from "fs";
 import { extrudeTilesetToImage } from "tile-extruder";
 
+const basePath = "public/assets/tilemaps/tilesets";
+
 async function main() {
-  const basePath = "public/assets/tilemaps/tilesets";
-  const tilesets = readdirSync(basePath).filter((file) => file.endsWith(".png"));
+  await extrude(`${basePath}`);
+  await extrude(`${basePath}/animations`);
+}
+
+async function extrude(input: string) {
+  const tilesets = readdirSync(input).filter((file) => file.endsWith(".png"));
 
   for (const tileset of tilesets) {
-    await extrudeTilesetToImage(32, 32, `${basePath}/${tileset}`, `${basePath}/extruded/${tileset}`);
+    await extrudeTilesetToImage(32, 32, `${input}/${tileset}`, `${input}/extruded/${tileset}`);
   }
-
-  console.log("Tilesets found: ", tilesets);
-
-  // await extrudeTilesetToImage(32, 32, "", "");
 }
 
 main();
